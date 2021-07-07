9db23a2095bb92c35de51331f85d8b5a0f2ad4f5
Mostafa Akram <mostafa@34ml.com>
2021-07-07 22:34:27 +0200

    Adjust footer

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -52,13 +52,8 @@
 
         <footer class="bg-white text-center text-sm mt-12 py-4" role="contentinfo">
             <ul class="flex flex-col md:flex-row justify-center list-none">
-                <li class="md:mr-2">
-                    &copy; <a href="https://tighten.co" title="Tighten website">Tighten</a> {{ date('Y') }}.
-                </li>
-
                 <li>
-                    Built with <a href="http://jigsaw.tighten.co" title="Jigsaw by Tighten">Jigsaw</a>
-                    and <a href="https://tailwindcss.com" title="Tailwind CSS, a utility-first CSS framework">Tailwind CSS</a>.
+                    Built with <a href="http://jigsaw.tighten.co" title="Jigsaw by Tighten">Jigsaw</a> <span>•</span> Hosted on <a href="https://www.netlify.com">Netlify</a>.
                 </li>
             </ul>
         </footer>

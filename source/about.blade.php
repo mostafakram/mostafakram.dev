b7c5659eea41988a7666a38dccd0bb6fb66fc42e
Mostafa Akram <mostafa@34ml.com>
2021-07-21 14:33:38 +0200

    Set about me page

--- source/about.blade.php
+++ source/about.blade.php
@@ -7,9 +7,8 @@ description: A little bit about the site
 @section('body')
     <h1>About</h1>
 
-    <img src="/assets/img/about.png"
-        alt="About image"
-        class="flex rounded-full h-64 w-64 bg-contain mx-auto md:float-right my-6 md:ml-10">
+    <div style="background-image: url('/assets/img/about.png')"
+         class="flex rounded-full h-64 w-64 bg-cover mx-auto md:float-right my-6 md:ml-10"></div>
 
     <p class="mb-6">This is where you can give a little more information about yourself or site. If you'd like to change the structure of this page, you can find the file at <code>source/about.blade.php</code></p>
 

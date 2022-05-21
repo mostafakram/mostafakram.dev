dcd7ebaac9ccea3fc79615dfd48bccdc364be6f6
Mostafa Akram <mostafa@iphonephotographyschool.com>
2022-05-21 14:18:43 +0200

    Remove about from navigation to update it later

--- source/_nav/menu-responsive.blade.php
+++ source/_nav/menu-responsive.blade.php
@@ -15,14 +15,6 @@
                 class="nav-menu__item hover:text-blue-500 {{ $page->isActive('/blog') ? 'active text-blue' : '' }}"
             >Timeline</a>
         </li>
-
-        <li class="pl-4">
-            <a
-                title="{{ $page->siteName }} About"
-                href="/about"
-                class="nav-menu__item hover:text-blue-500 {{ $page->isActive('/about') ? 'active text-blue' : '' }}"
-            >About</a>
-        </li>
         <li class="pl-4">
             <a
                 title="{{ $page->siteName }} Contact"
